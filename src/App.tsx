55a2660dc0c0db36d9415a88e9396068b5f5296e
Solest <zkordo044@gmail.com>
2023-10-01 22:42:55 -0400

    Completed underlying simulation logic. Ready to implement drawing and UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Layer, Rect, Stage } from 'react-konva'
 
 function App() {
 
+//TODO: 1) Get canvas implemented. 2) Make sure falling object draws correctly. 3) End sim if object hits ground or at least stop it from moving. 4) Probably make environment just a state variable. 5) fallingObjects array also state variable?
   return (
     <>
-
+      <canvas id="canvas" />
     </>
   )
 }
